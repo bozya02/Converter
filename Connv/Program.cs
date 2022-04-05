096f0ac1a8cd26b324362bf4b58968387acf0200
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2022-04-05 14:14:53 +0300

    Add project files.

---
+++ Connv/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new ObservableCollection<Product>(DBConnection.connection.Products);
+
+            var images = Directory.GetFiles(@"\\nas36d451\user-domain$\stud\201906\Desktop\Product");
+
+            foreach (var image in images)
+            {
+                string name = image.Split('\\')[8].Split('.')[0];
+                Product prod = products.Where(p => p.Name == name).FirstOrDefault();
+
+                try
+                {
+                    prod.Photo = File.ReadAllBytes(images.FirstOrDefault(p => p.Contains(prod.Name)));
+                }
+                catch
+                {
+                    Console.WriteLine($"Error {name}");
+                }
+
+                DBConnection.connection.Products.SingleOrDefault(p => p.Name == name);
+                Console.WriteLine(DBConnection.connection.SaveChanges());
+            }
+        }
+    }
+}
